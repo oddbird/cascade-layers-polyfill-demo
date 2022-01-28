55d5789c1e124e122ad96c189c70594f1342a45f
Olu Niyi-Awosusi <hello@olu.online>
2022-01-28 21:32:51 +0000

    initial commit'

---
+++ index.js
@@ -0,0 +1,21 @@
+let layerTest = document.createElement("style");
+let passesTest = false;
+layerTest.innerHTML = `@layer browser-syntax-test {
+  :root {
+    --test: testing;
+  }
+}`;
+// Get the first script tag
+var ref = document.querySelector("script");
+
+// Insert our new styles before the first script tag
+ref.parentNode.insertBefore(layerTest, ref);
+
+if (
+  window
+    .getComputedStyle(document.documentElement)
+    .getPropertyValue("--test")
+    .trim() === "testing"
+) {
+  passesTest = true;
+}

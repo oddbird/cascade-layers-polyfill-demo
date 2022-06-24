f7ab0c6776dd61a8f5442478432ec7e3b3f8e18d
Jonny Gerig Meyer <jonny@oddbird.net>
2022-06-24 18:10:39 -0400

    WIP generating separate webpack css bundle

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 // set up webpack or similar?
-const styles = require('./index.css')
+// const styles = require('./index.css')
 
-postcss([
-	postcssCascadeLayers(/* pluginOptions */)
-]).process(styles /*, processOptions */);
+// postcss([
+// 	postcssCascadeLayers(/* pluginOptions */)
+// ]).process(styles /*, processOptions */);
 
-let layerTest = document.createElement("style");
+let layerTest = document.createElement('style');
 let passesTest = false;
 layerTest.innerHTML = `@layer browser-syntax-test {
   :root {
@@ -13,7 +13,7 @@ layerTest.innerHTML = `@layer browser-syntax-test {
   }
 }`;
 // Get the first script tag
-var ref = document.querySelector("script");
+var ref = document.querySelector('script');
 
 // Insert our new styles before the first script tag
 ref.parentNode.insertBefore(layerTest, ref);
@@ -21,14 +21,12 @@ ref.parentNode.insertBefore(layerTest, ref);
 if (
   window
     .getComputedStyle(document.documentElement)
-    .getPropertyValue("--test")
-    .trim() === "testing"
+    .getPropertyValue('--test')
+    .trim() === 'testing'
 ) {
   passesTest = true;
 }
 
 if (passesTest) {
-
 } else {
-
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,41 @@
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const plugins = [new MiniCssExtractPlugin()];
 
 module.exports = {
-	plugins,
-	module: {
-		rules: [
-			{
-				test: /\.css$/i,
-				use: [
-					MiniCssExtractPlugin.loader,
-          "css-loader",
-          "postcss-loader",
-					{
-						loader: 'css-loader',
-						options: {
-							importLoaders: 1,
-						}
-					},
-					{
-						loader: "postcss-loader",
-						options: {
-							postcssOptions: {
-								plugins: [
-									[
-										"@csstools/postcss-cascade-layers",
-										{
-											// Options
-										},
-									],
-								],
-							},
-						},
-					},
-				],
-			},
-		],
-	},
+  entry: {
+    app: ['./src/index.js', './src/index.css'],
+  },
+  plugins,
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [
+                  [
+                    '@csstools/postcss-cascade-layers',
+                    {
+                      // Options
+                    },
+                  ],
+                ],
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
 };

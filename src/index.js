f385f9a316ca99796f91660240e6cd3f5a510df9
Jonny Gerig Meyer <jonny@oddbird.net>
2023-07-17 15:39:37 -0400

    Check for layer support

--- src/index.js
+++ src/index.js
@@ -22,11 +22,44 @@ inputs.forEach(function (i) {
       case 'plain':
         removeCSS('polyfilled');
         addCSS('plain');
+        document.documentElement.classList.remove('polyfilled');
         break;
       case 'polyfilled':
         removeCSS('plain');
         addCSS('polyfilled');
+        document.documentElement.classList.add('polyfilled');
         break;
     }
   });
 });
+
+// the support function
+// - returns true or false depending on support
+// - sets a class on the root element
+// @see https://codepen.io/miriamsuzanne/pen/poLOYrz
+const checkLayerSupport = () => {
+  const varName = '--js-test-layer-support';
+
+  // inject a style element with a layered variable
+  const style = document.createElement('style');
+  style.textContent = `@layer {:root{${varName}: true;}}`;
+  document.head.appendChild(style);
+
+  // check if the variable is set
+  const hasSupport = getComputedStyle(document.documentElement)
+    .getPropertyValue(varName)
+    .includes('true');
+
+  // remove the style element
+  document.head.removeChild(style);
+
+  // add a root class
+  const supportClass = hasSupport ? 'layers-true' : 'layers-false';
+  document.documentElement.classList.add(supportClass);
+
+  // return true if supported
+  return hasSupport;
+};
+
+// check for @layer support
+checkLayerSupport();
